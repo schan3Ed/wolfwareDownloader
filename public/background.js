02f3dfda1d406fc00f4f8689cdb9211ed97ad969
Evan Li <evan.li.2010@hotmail.com>
2017-09-04 19:08:30 -0400

    Code refactor, tab title check adjustment, linting

--- public/background.js
+++ public/background.js
@@ -1,70 +1,40 @@
-// import { getCache } from '../src/stores/UserConfigStore';
-// Do I even need this?
+/* global chrome */
 chrome.browserAction.setIcon({
-  path: "icon.png"
+  path: 'icon.png',
 });
 
-//list of sites that needed to be check
-let checkSites = function(downloadItem) {
-  let url = downloadItem.url;
-  return checkWolfware(url);
-}
-
-//checking for wolfware
-let checkWolfware = function(url) {
-  if (url.indexOf('wolfware') != -1) {
-    //setting up for wolfware environment
-    startIndex = 0;
-    endIndex = 6;
-    return true;
-  }
-  return false;
-};
-
+// list of sites that needed to be check
 function getFromStorageSync(key) {
-  return new Promise(function (resolve) {
-    chrome.storage.sync.get(key, function (items) {
-      resolve(items)
-    })
-  })
+  return new Promise((resolve) => {
+    chrome.storage.sync.get(key, (items) => {
+      resolve(items);
+    });
+  });
 }
 
-
-let startIndex = 0;
-let endIndex = 0;
-
-
-//Where the determining file name starts  
-chrome.downloads.onDeterminingFilename.addListener(function(downloadItem, suggest) {
-  if (checkSites(downloadItem)) {
-    chrome.tabs.query({
-      active: true,
-      lastFocusedWindow: true
-    }, function(tabs) {
- //     let tabTitle = tabs[0].title;
-      // This right here get the right title in any page v0.1.3
-      // Should extract this as a refactor if we have more websites. But for now, its okay
-      chrome.tabs.executeScript({
-          code: 'document.getElementsByClassName("page-header-headings")[0].innerText'
-      }, function(result) {
-        let pathName = "";
-        console.log(result[0])
-   //     result = result.textContent
-        pathName += result[0].substring(startIndex, endIndex);
-        getFromStorageSync('config').then((result) => {
-          console.log(result.config)
+chrome.downloads.onDeterminingFilename.addListener((downloadItem, suggest) => {
+  // Add more sites potentially
+  if (downloadItem.url.includes('wolfware')) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.executeScript(tabs[0].id, {
+        code: 'document.getElementsByClassName("page-header-headings")[0].innerText',
+      }, (result) => {
+        const pathName = result[0].split(/[\s]+/);
+        const courseTitle = pathName[0];
+        const courseNumber = pathName[1];
+        getFromStorageSync('config').then((storage) => {
           suggest({
-            filename: pathName + '/' + downloadItem.filename,
-            conflictAction: result.config
+            filename: `${courseTitle}${courseNumber}/${downloadItem.filename}`,
+            conflictAction: storage.config,
           });
-        })
+        });
       });
     });
-  } else
+  } else {
     suggest({
       filename: downloadItem.filename,
-      conflictAction: 'uniquify'
+      conflictAction: 'uniquify',
     });
-
+  }
   return true;
 });
